6d76378e427926504a49da0a4de72ddfc73071dc
Gustavo Vono <102389423+gutvono@users.noreply.github.com>
2024-06-18 11:04:23 -0300

    feat: new service methods for cpf validation
    - ValidateCpf;
    - ValidateFirstDigit;
    - ValidateSecontDigit;

--- OnTheFly.PassengersAPI/Services/CreatePassengerService.cs
+++ OnTheFly.PassengersAPI/Services/CreatePassengerService.cs
@@ -37,6 +37,8 @@ namespace Services
 
         public async Task<Passenger> CreatePassenger(PassengerDTO passengerDTO, Address address)
         {
+            if (!ValidateCpf(passengerDTO.Cpf)) throw new Exception("CPF inválido.");
+
             var passenger = new Passenger
             {
                 Cpf = passengerDTO.Cpf,
@@ -50,6 +52,54 @@ namespace Services
             };
 
             return passenger;
+        }
+
+        public static bool ValidateCpf(string cpf)
+        {
+            if (cpf.Contains(".")) cpf = cpf.Replace(".", "");
+            if (cpf.Contains("-")) cpf = cpf.Replace("-", "");
+
+            if (cpf.Length != 11) return false;
+
+            bool valid = false;
+            for (int i = 0; i < cpf.Length - 1 && !valid; i++)
+            {
+                int n1 = int.Parse(cpf.Substring(i, 1));
+                int n2 = int.Parse(cpf.Substring(i + 1, 1));
+
+                if (n1 != n2) valid = true;
+            }
+
+            return valid && ValidateFirstDigit(cpf) && ValidateSecondDigit(cpf);
+        }
+
+        private static bool ValidateFirstDigit(string cpf)
+        {
+            int result = 0;
+            for (int i = 0, mult = 10; i < 9; i++, mult--) result += int.Parse(cpf.Substring(i, 1)) * mult;
+
+            int rest = (result * 10) % 11;
+            if (rest == 10) rest = 0;
+
+            int FirstDigit = int.Parse(cpf.Substring(9, 1));
+
+            if (rest == FirstDigit) return true;
+
+            return false;
+        }
+
+        private static bool ValidateSecondDigit(string cpf)
+        {
+            int result = 0;
+            for (int i = 0, mult = 11; i < 10; i++, mult--) result += int.Parse(cpf.Substring(i, 1)) * mult;
+
+            int rest = (result * 10) % 11;
+
+            int SecondDigit = int.Parse(cpf.Substring(10, 1));
+
+            if (rest == SecondDigit) return true;
+
+            return false;
         }
     }
 }
